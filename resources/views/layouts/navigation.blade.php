4d891afe4a35efd14048e93dd6cbf5caf96833f4
rtrwslv <83144402+rtrwslv@users.noreply.github.com>
2023-09-20 18:54:05 +0300

    Тест

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -35,7 +35,7 @@
 
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Профиль') }}
+                            {{ __('Хуефиль') }}
                         </x-dropdown-link>
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Новое объявление') }}
