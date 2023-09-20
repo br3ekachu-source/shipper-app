0feeedcd9c44408a7bba56beb3bc493efe9c7643
br3ekachu <br3ekachu@gmail.com>
2023-09-20 18:55:54 +0300

    Тесссст

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -35,7 +35,7 @@
 
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Хуефиль') }}
+                            {{ __('пиздец Хуефиль') }}
                         </x-dropdown-link>
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Новое объявление') }}
