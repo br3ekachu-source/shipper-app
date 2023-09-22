341db38c585bce074179790c4da2feb8450b5d12
br3ekachu <br3ekachu@gmail.com>
2023-09-22 17:21:11 +0300

    jпправил жиесы

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,22 +12,5 @@
             </div>
         </div>
     </div>
-    <script>
-        function generateCards(num) {
-    let cards = '';
-    for (let i = 0; i < num; i++) {
-      cards += `
-        <div class="bg-white border rounded-lg p-4 mt-6 ml-3 mr-3">
-          <img src="https://i.imgur.com/qb3q2Sp.png" alt="Boat" class="w-full object-cover h-16 sm:h-48 md:h-16 lg:h-48" />
-          <h3 class="mt-4 text-xl font-semibold">Сыкс с лодкой</h3>
-          <p class="text-gray-600">Ахуеть сыкс с лодкой</p>
-          <p class="mt-3 font-bold text-lg">1400000 ₽</p>
-        </div>
-      `;
-    }
-    return cards;
-  }
-
-  document.getElementById('generatedCards').innerHTML = generateCards(40);
-    </script>
+    @vite(['resources/js/generate_cards.js'])
 </x-app-layout>
