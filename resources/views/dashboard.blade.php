c273c25beecf7350cb560a30fc291fd3ab4ce383
rtrwslv <83144402+rtrwslv@users.noreply.github.com>
2023-09-23 02:25:44 +0300

    Ability to put likes, spinning pics (DOHYYA SDELAL)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,11 +4,11 @@
             <input type="text" class="form-input rounded-md shadow-sm block w-full" placeholder="Поиск...">
         </div>
     </x-slot>
-
+    
     <div class="py-6">
-        <div class="max-w-15xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div id="generatedCards" class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-4 gap-8"></div>
+                <div id="generatedCards" class="grid grid-cols-4 md:grid-cols-4 lg:grid-cols-4 gap-8 p-4"></div>
             </div>
         </div>
     </div>
