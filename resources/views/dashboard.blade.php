daef8efefad4da37f59ddd597bbbdce9ea4ca4cf
br3ekachu <br3ekachu@gmail.com>
2023-10-07 16:59:32 +0300

    Revert "Merge branch 'main' of https://github.com/br3ekachu-source/shipper-app"
    This reverts commit b3f470e003a95ee5498d539b61f5ec4213d13d29, reversing
    changes made to 5d7dc93ddf35af0a3b4a60fc5e8b63a2a78dbb8c.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div id="generatedCards" class="grid grid-cols-4 md:grid-cols-4 lg:grid-cols-1 gap-1 p-4"></div>
+                <div id="generatedCards" class="grid grid-cols-4 md:grid-cols-4 lg:grid-cols-4 gap-8 p-4"></div>
             </div>
         </div>
     </div>
